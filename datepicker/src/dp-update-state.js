2110e0f5c5eb29386bd70906bb1e7192bb0e49ff
Olga Shved <shved.volha@gmail.com>
2019-08-20 17:45:32 +0300

    Fix in DPUpdateState

--- datepicker/src/dp-update-state.js
+++ datepicker/src/dp-update-state.js
@@ -4,12 +4,12 @@ export default class DPUpdateState {
   constructor(isOpened, calendarDate, selectedDate, minDate, maxDate) {
     this.isOpened = isOpened;
 
-    if(!calendarDate instanceof Date) {
+    if(calendarDate && !(calendarDate instanceof Date)) {
       throw new DateFormatError(`Calendar date is type of ${calendarDate.constructor.name} but expected type is Date`);
     }
     this.calendarDate = calendarDate;
 
-    if(!selectedDate && !selectedDate instanceof Date) {
+    if(selectedDate && !(selectedDate instanceof Date)) {
       throw new DateFormatError(`Selected date is type of ${selectedDate.constructor.name} but expected type is Date`);
     }
     else if(!selectedDate) {
@@ -17,12 +17,12 @@ export default class DPUpdateState {
     }
     this.selectedDate = selectedDate;
 
-    if(minDate && !minDate instanceof Date) {
+    if(minDate && !(minDate instanceof Date)) {
       throw new DateFormatError(`Min date is type of ${minDate.constructor.name} but expected type is Date`);
     }
     this.minDate = minDate;
 
-    if(maxDate && !maxDate instanceof Date) {
+    if(maxDate && !(maxDate instanceof Date)) {
       throw new DateFormatError(`Max date is type of ${maxDate.constructor.name} but expected type is Date`);
     }
     this.maxDate = maxDate;
